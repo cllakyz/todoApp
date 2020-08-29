879bf299eff11519f2b5bd69438ff2a9a47ee3fe
Celal <celal7174@gmail.com>
2020-08-29 17:56:32 +0300

    readme

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,21 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $admin = User::create([
+        User::create([
             'name'      => 'Admin',
             'email'     => 'admin@test.com',
             'password'  => Hash::make('admin'),
             'role'      => 'admin'
         ]);
-        /*$user = User::create([
-            'name'      => 'User',
-            'email'     => 'user@test.com',
-            'password'  => Hash::make('secret'),
-            'role'      => 'user'
-        ]);
-
-        factory(Todo::class, 5)->create(['user_id' => $admin->id]);
-        factory(Todo::class, 5)->create(['user_id' => $user->id]);*/
 
         factory(User::class, 5)->create()->each(function ($user) {
             factory(Todo::class, 10)->create(['user_id' => $user->id]);
